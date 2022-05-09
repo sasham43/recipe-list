67767c5be73d4d4da79928e8e5e976cce1624713
Sasha Kramer <sashasemail@gmail.com>
2022-05-09 17:55:53 -0400

    added ingredients

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,5 +1,6 @@
+import { useState, } from 'react'
 import { useForm } from '@mantine/form'
-import { TextInput, Button } from '@mantine/core'
+import { Group, TextInput, Button } from '@mantine/core'
 import { DatePicker } from '@mantine/dates'
 
 
@@ -9,16 +10,32 @@ export default function Create(){
             name: '',
             created: null,
             creator: '',
-            ingredients: [{
-                quantity: '',
-                name: '',
-            }],
-            steps: [''],
+            ingredients: [],
+            steps: [],
         },
     })
 
     function handleSubmit(values){
         console.log('values', values)
+    }
+
+    const [newIngredientQuantity, setNewIngredientQuantity] = useState('')
+    const [newIngredientName, setNewIngredientName] = useState('')
+
+    function onQuantity(value){
+        setNewIngredientQuantity(value)
+    }
+    function onName(value){
+        setNewIngredientName(value)
+    }
+    function addIngredient(quantity, name){
+        form.setFieldValue('ingredients', [...form.values.ingredients, {quantity, name}])
+        console.log('form', form)
+        setNewIngredientQuantity('')
+        setNewIngredientName('')
+    }
+    function removeIngredient(index) {
+        form.setFieldValue('ingredients', form.values.ingredients.filter((ing, i) => i != index))
     }
 
     return (
@@ -42,8 +59,41 @@ export default function Create(){
                     placeholder="Rosie Jones"
                     {...form.getInputProps('creator')}
                 />
+                <div>
+                    <div>Ingredients</div>
+                    <ul>
+                        {
+                            form?.values.ingredients?.map((ingredient, index) => {
+                                return (
+                                    <li key={`ingredient-${index}`}>
+                                        <span>
 
-                <Button type="submit">Save</Button>
+                                            {ingredient.quantity} {ingredient.name}
+                                        </span>
+                                        <Button onClick={()=>removeIngredient(index)}>Remove</Button>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                    <div className={'ingredients'}>
+                        <TextInput
+                            name="Ingredient Quantity"
+                            value={newIngredientQuantity}
+                            onChange={(e) => onQuantity(e.target.value)}
+                        />
+                        <TextInput
+                            name="Ingredient Name"
+                            value={newIngredientName}
+                            onChange={(e) => onName(e.target.value)}
+                        />
+                        <Button onClick={() => addIngredient(newIngredientQuantity, newIngredientName)}>Add</Button>
+                    </div>
+                </div>
+
+                <Group>
+                    <Button type="submit">Save</Button>
+                </Group>
             </form>
         </>
     )

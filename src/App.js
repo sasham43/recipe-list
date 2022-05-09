1ba99058831ce8c94cafbed4ee37c2754733f2c1
Sasha Kramer <sashasemail@gmail.com>
2022-05-09 15:28:44 -0400

    stub out router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 import List from './components/List'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import Details from './components/Details'
+import Create from './components/Create'
 
 function App() {
   return (
     <div className="App">
-      <List />
+      {/* <List /> */}
+      <Router>
+        <Routes>
+          <Route exact path="/" >
+            <Route index element={<List />}></Route>
+            <Route path="/details" element={<Details />}>
+            </Route>
+            <Route path="/create" element={<Create />}>
+            </Route>
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }

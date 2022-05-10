c5ed5506a913cf91855a5a5bc9a6c8b2ba9b2c11
Sasha Kramer <sashasemail@gmail.com>
2022-05-10 15:29:49 -0400

    fix lists on small screens

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { Card } from '@mantine/core'
 
 import { getRecipes } from '../recipes'
 import Favorite from './Favorite'
@@ -23,54 +24,56 @@ export default function Details(){
 
     return (
         <>
-            <h1>Details</h1>
-            {recipe ? <div>
-                <h2 className={`recipe-item`}>{recipe.name}</h2>
-                <div className={`recipe-item`}>
-                    <span>Cooking Time: </span> <span>{recipe.cookingTime} hours</span>
-                </div>
-                <div className={`recipe-item`}>
-                    {recipe.creator}
-                </div>
-                <div className={`recipe-item`}>
-                    {recipe.createdDate.format('MMMM D YYYY h a')}
-                </div>
-                <div className={`recipe-item`}>
-                    {/* <button>
-                        Favorite
-                    </button> */}
-                    <Favorite index={params.index} />
-                </div>
-                <div className={`recipe-item`}>
-                    <h3>Ingredients</h3>
-                    <ul>
-                        {
-                            recipe.ingredients.map((ingredient, index) => {
-                                return  (
-                                    <li key={`ingredient-${index}`}>
-                                        <span className={`ingredient-quantity`}>{ingredient.quantity}</span>
-                                        <span>{ingredient.name}</span>
-                                    </li>
-                                )
-                            })
-                        }
-                    </ul>
-                </div>
-                <div className={`recipe-item`}>
-                    <h3>Steps</h3>
-                    <ol>
-                        {
-                            recipe.steps.map((step, index) => {
-                                return  (
-                                    <li key={`step-${index}`}>
-                                        {step}
-                                    </li>
-                                )
-                            })
-                        }
-                    </ol>
-                </div>
-            </div> : null}
+            <Card shadow="sm" className={`page-card`}>
+                <h1>Details</h1>
+                {recipe ? <div>
+                    <h2 className={`recipe-item`}>{recipe.name}</h2>
+                    <div className={`recipe-item`}>
+                        <span>Cooking Time: </span> <span>{recipe.cookingTime} hours</span>
+                    </div>
+                    <div className={`recipe-item`}>
+                        {recipe.creator}
+                    </div>
+                    <div className={`recipe-item`}>
+                        {recipe.createdDate.format('MMMM D YYYY h a')}
+                    </div>
+                    <div className={`recipe-item`}>
+                        {/* <button>
+                            Favorite
+                        </button> */}
+                        <Favorite index={params.index} />
+                    </div>
+                    <div className={`recipe-card recipe-item`}>
+                        <h3>Ingredients</h3>
+                        <ul>
+                            {
+                                recipe.ingredients.map((ingredient, index) => {
+                                    return  (
+                                        <li key={`ingredient-${index}`}>
+                                            <span className={`ingredient-quantity`}>{ingredient.quantity}</span>
+                                            <span>{ingredient.name}</span>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                    <div className={`recipe-card recipe-item`}>
+                        <h3>Steps</h3>
+                        <ol>
+                            {
+                                recipe.steps.map((step, index) => {
+                                    return  (
+                                        <li key={`step-${index}`}>
+                                            {step}
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ol>
+                    </div>
+                </div> : null}
+            </Card>
         </>
     )
 }
